d3c6a296ef870d1f465e52bda2f49a12e8f9801f
thiago <nasciutti.thiago@gmail.com>
2022-09-16 14:02:12 -0400

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,31 @@
+var apiKey = "cd1412ef701a22c31ed33a851981b439";
+var cityEl = document.querySelector("#city");
+var btnEl = document.querySelector("#btn");
+var cityLat;
+var cityLon;
 
-let cityEl = document.getElementById("city").value;
-console.log(cityEl)
-// function mainFn() {
- 
-
-//  //  Coordinates API
-// let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityVal}&limit=1&appid=${apiKey}`;
-//  fetch(url)
-//   .then((res) => {
-//    return res.json();
-//   })
-//   .then((data) => {
-//    let cityLat = data[0].lat;
-//    let cityLon = data[0].lon;
-//   });
-//   console.log(cityLat)
-// }
+function mainFn() {
+ //  Coordinates API
+ let cityVal = cityEl.value;
+ var url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityVal}&limit=1&appid=${apiKey}`;
+ fetch(url)
+  .then((res) => {
+   return res.json();
+  })
+  .then((data) => {
+  cityLat = data[0].lat;
+  cityLon = data[0].lon;
+  });
+}
 
 
+function test(){
+  console.log(cityLat)
+}
 
 
-// // Listener
-// btnEl.addEventListener("click", mainFn);
+test();
+
+
+// Listener
+btnEl.addEventListener("click", mainFn);
