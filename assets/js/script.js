f01e84bd05dbe23930fcbe5dac47739a35724a91
thiago <nasciutti.thiago@gmail.com>
2022-09-16 11:31:47 -0400

    fetch-error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-//const apiKey = "cd1412ef701a22c31ed33a851981b439";
-// var cityEl = document.querySelector("#city");
-// var btnEl = document.querySelector("#btn");
+const apiKey = "cd1412ef701a22c31ed33a851981b439";
+var cityEl = document.querySelector("#city");
+var btnEl = document.querySelector("#btn");
 
-// function mainFn() {
-//  var cityVal = cityEl.value;
+function mainFn() {
+ var cityVal = cityEl.value;
 
 
 //  Coordinates API
- var url = `http://api.openweathermap.org/geo/1.0/direct?q=london&limit=1&appid=cd1412ef701a22c31ed33a851981b439`;
+ var url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityVal}&limit=1&appid=${apiKey}`;
  fetch(url)
   .then(function (response) {
    return response.json();
@@ -15,20 +15,7 @@
   .then(function (data) {
    console.log(data.lat)
   });
+} 
 
-//  //Weather API
-//  var url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API key}`;
-
-//  fetch(url)
-//  .then(function(response){
-//          //gets response ready
-//          return response.json();
-//      })
-//  .then(function(data){
-//          //‘response’ is ready and is available as ‘data’.
-//          console.log(data);
-//      });
-// }
-
-// // Listener
-// btnEl.addEventListener("click", mainFn);
+// Listener
+btnEl.addEventListener("click", mainFn);
