722a06ac17e817fcb6d282bfd8db7ef76319cc83
thiago <nasciutti.thiago@gmail.com>
2022-09-16 13:38:36 -0400

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
-const apiKey = "cd1412ef701a22c31ed33a851981b439";
-var cityEl = document.querySelector("#city");
-var btnEl = document.querySelector("#btn");
 
-function mainFn() {
- var cityVal = cityEl.value;
+let cityEl = document.getElementById("city").value;
+console.log(cityEl)
+// function mainFn() {
+ 
+
+//  //  Coordinates API
+// let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityVal}&limit=1&appid=${apiKey}`;
+//  fetch(url)
+//   .then((res) => {
+//    return res.json();
+//   })
+//   .then((data) => {
+//    let cityLat = data[0].lat;
+//    let cityLon = data[0].lon;
+//   });
+//   console.log(cityLat)
+// }
 
 
-//  Coordinates API
- var url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityVal}&limit=1&appid=${apiKey}`;
- fetch(url)
-  .then(function (response) {
-   return response.json();
-  })
-  .then(function (data) {
-   console.log(data.lat)
-  });
-} 
 
-// Listener
-btnEl.addEventListener("click", mainFn);
+
+// // Listener
+// btnEl.addEventListener("click", mainFn);
